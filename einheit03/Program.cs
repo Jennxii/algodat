d05567244ea2c4b5ff453f9a4fb86df0933e6a64
Jennifer Strohmer <jstrohmer.mmt-b2022@fh-salzburg.ac.at>
2023-03-24 09:31:25 +0100

    added einheit03

---
+++ einheit03/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using (StreamReader rd = new StreamReader("input.txt"))
+{
+    string klammerline = null;
+
+    while ((klammerline = rd.ReadLine()) != null)
+    {
+
+
+        Console.WriteLine(klammerline);
+
+    }
+
+
+}
+
+
+
+
